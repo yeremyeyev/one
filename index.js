90a5cef968c9a14d8519fd7b7c148b27dbee7328
Eremeev Andrey <a.yeremyeyev@gmail.com>
2020-01-09 14:28:00 +0200

    09.01.2020
    отправка письма не получилась

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// подключаю fs
+const fs = require('fs');
+//записиваю в фал
+let data = fs.writeFileSync('data.txt', 'Привет мир!');
+
+//дописываю в фал
+let data_to_append = 'Дописать текст с новой строки, не затирая предидущий';
+fs.appendFileSync('data.txt', `\n${data_to_append}`);
+
+//читаю что дописал 
+let dataContent = fs.readFileSync('data.txt', 'utf8');
+console.log(dataContent);
+
+//шлю письмо
+// const nodemailer = require('nodemailer');
+    
+// let testEmailAccount = await nodemailer.createTestAccount();
+
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'a.yeremyeyev@gmail.com',
+//         pass: 'pass'
+//       }
+//     });
+
+// let result = await transporter.sendMail({
+//   from: '"Node js" <a.yeremyeyev@gmail.com>',
+//   to: "a.yeremyeyev@gmail.com",
+//   subject: "Message from Node js",
+//   text: "This message was sent from Node js server.",
+//   html: "This <i>message</i> was sent from <strong>Node js</strong> server."
+// });
+
+// console.log(result);
